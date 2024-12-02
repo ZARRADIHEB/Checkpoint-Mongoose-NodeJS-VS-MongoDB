fcc3ee8f33fdd66d713ef10a5b90cd7baff666e8
iZ17 <zarradiheb007@gmail.com>
2024-12-02 07:00:18 +0100

    second commit

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
-//?Import mongoose module
-const mongoose = require("mongoose");
-//?Import dotenv module
-const dotenv = require("dotenv");
-//?Import person module
-const Person = require("./model");
+// //?Import mongoose module
 
-//? Load environment variables
-dotenv.config();
+// const mongoose = require("mongoose");
+// //?Import dotenv module
 
-//?Get the MongoDB URI from the .env file
-const mongoURI = process.env.mongo_URI;
+// const dotenv = require("dotenv");
+// //?Import person module
 
-//? Connect to the MongoDB database
-const connectToMongoDB = async () => {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB successfully 😍");
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
-  }
-};
+// const Person = require("./model");
 
-// Call the function to connect
-connectToMongoDB();
+// //? Load environment variables
+
+// dotenv.config();
+
+// ////?Get the MongoDB URI from the .env file
+
+// const mongoURI = process.env.mongo_URI;
+
+// //? Connect to the MongoDB database
+
+// const connectToMongoDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI);
+//     console.log("Connected to MongoDB successfully 😍");
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err.message);
+//   }
+// };
+
+// // Call the function to connect
+
+// connectToMongoDB();
+
+// //? Create one record
 
 // const newPerson = async () => {
 //   try {
@@ -43,7 +52,8 @@ connectToMongoDB();
 
 // newPerson();
 
-// ? Data to create many records
+// //? Data to create many records
+
 // const peopleData = [
 //   {
 //     name: "Alice",
@@ -65,7 +75,8 @@ connectToMongoDB();
 //   },
 // ];
 
-//?Create many records at once
+// //?Create many records at once
+
 // const createManyPeople = async () => {
 //   try {
 //     const createdPeople = await Person.create(peopleData);
@@ -77,32 +88,36 @@ connectToMongoDB();
 //   }
 // };
 
-//?Call the function
+// //?Call the function
+
 // createManyPeople();
 
-//? Find People
+// //? Find People
+
 // const findPeople = async (name) => {
-//In case everything goes perfect
-// try {
-//   const result = await Person.find({ name: name });
-//   if (result) {
-//     console.log("Found person:", result);
-//   } else {
-//     console.log("No person found with that favorite food.");
-//   }
-// In case an error occured
-// } catch (error) {
-//   console.error("Error finding people:", error.message);
-// Close the connection after the operation
+//   //In case everything goes perfect
+//   try {
+//     const result = await Person.find({ name: name });
+//     if (result) {
+//       console.log("Found person:", result);
+//     } else {
+//       console.log("No person found with that favorite food.");
+//     }
+//     //In case an error occured
+//   } catch (error) {
+//     console.error("Error finding people:", error.message);
+//     //Close the connection after the operation
 //   } finally {
 //     mongoose.connection.close();
 //   }
 // };
 
-//?Call the function
+// //?Call the function
+
 // findPeople("Iheb");
 
-//? Find One Person
+// //? Find One Person
+
 // const findPerson = async (age) => {
 //   try {
 //     const result = await Person.findOne({ age: age });
@@ -120,7 +135,8 @@ connectToMongoDB();
 
 // findPerson(25);
 
-//? Find By Id
+// //? Find By Id
+
 // const findById = async (id) => {
 //   try {
 //     const result = await Person.findById(id);
@@ -138,7 +154,8 @@ connectToMongoDB();
 
 // findById("674cb1f2c53be47c7ae70398");
 
-//? find by id and add to the array
+// //? find by id and add item to the array
+
 // const findAndAdd = async (id, item) => {
 //   try {
 //     const target = await Person.findById(id);
@@ -158,7 +175,8 @@ connectToMongoDB();
 
 // findAndAdd("674cb1f2c53be47c7ae7039a", "burritos");
 
-//? find one and update
+// //? find one and update
+
 // const findOneAndUpdate = async (name) => {
 //   try {
 //     const result = await Person.findOneAndUpdate(
@@ -181,16 +199,17 @@ connectToMongoDB();
 
 // findOneAndUpdate("Charlie");
 
-//? find by id and remove
+// //? find by id and remove
+
 // const findByIdAndDelete = async (id) => {
 //   try {
-// Check if the ID is valid
-// if (!mongoose.Types.ObjectId.isValid(id)) {
-//   console.log("Invalid ID format");
-//   return;
-// }
+//     //Check if the ID is valid
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       console.log("Invalid ID format");
+//       return;
+//     }
 
-// Attempt to find and remove the document
+//     //Attempt to find and remove the document
 //     const result = await Person.findByIdAndDelete(id);
 //     if (!result) {
 //       console.log("No user found with the given ID");
@@ -207,7 +226,8 @@ connectToMongoDB();
 
 // findByIdAndDelete("674cb5e8271e63d1013d6f1d");
 
-// Delete many documents
+// // Delete many documents
+
 // const deletePeopleNamedMary = async () => {
 //   try {
 //     const result = await Person.deleteMany({ name: "Mary" });
@@ -221,7 +241,8 @@ connectToMongoDB();
 
 // deletePeopleNamedMary();
 
-//? Chain Search Query Helpers to Narrow Search Results
+// //? Chain Search Query Helpers to Narrow Search Results
+
 // const findPeopleWhoLikeBurritos = async () => {
 //   try {
 //     const data = await Person.find({ favoriteFoods: "burritos" }) // Find people who like burritos
@@ -238,4 +259,5 @@ connectToMongoDB();
 // };
 
 // // Run the function
+
 // findPeopleWhoLikeBurritos();
